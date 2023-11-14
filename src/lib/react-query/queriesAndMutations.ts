06106865d50062b88568e660ca8601ebe017d8e3
Moataz0 <zo.o@hotmail.com>
2023-11-14 14:51:56 +0200

    Update login

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -6,19 +6,22 @@ import {
   QueryClientProvider,
   useInfiniteQuery,
 } from "@tanstack/react-query";
-import { createUserAccount, signInAccount } from "../appwrite/api";
+import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 import { INewUser } from "@/types";
 
 export const useCreateUserAccount = () => {
-  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (user: INewUser) => createUserAccount(user),
   });
 };
 export const useSignInAccount = () => {
-  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (user: { email: string; password: string }) =>
     signInAccount(user),
+  });
+};
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
   });
 };
